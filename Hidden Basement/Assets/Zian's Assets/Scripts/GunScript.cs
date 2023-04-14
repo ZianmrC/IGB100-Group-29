90fa9800494bd148cdb5dbeb33c9355885fd20fa
ZianmrC <116824051+ZianmrC@users.noreply.github.com>
2023-04-14 16:19:37 +1000

    Added Basic Player Shooting

--- Hidden Basement/Assets/Zian's Assets/Scripts/GunScript.cs
+++ Hidden Basement/Assets/Zian's Assets/Scripts/GunScript.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GunScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject bulletPrefab;         // Prefab of the bullet to shoot
+    public Transform firePoint;            // Transform representing the position where the bullet should be spawned
+    public float range = 100f;             // Maximum range of the bullet
+    public int damage = 10;               // Damage of the bullet
+    public float fireRate = 2f;         // Rate of fire in seconds (e.g. 0.1f means 10 bullets per second)
+    public float projectileVelocity = 50f; // Velocity of the bullet
 
-    // Update is called once per frame
+    private float nextFireTime;          // Time of the next allowed shot
+
     void Update()
     {
-        
+        // Check for fire input (e.g. left mouse button)
+        if (Input.GetButton("Fire1") && Time.time >= nextFireTime)
+        {
+            // Shoot a bullet
+            Shoot();
+            // Update the next allowed shot time
+            nextFireTime = Time.time + 1f / fireRate;
+        }
     }
+
+    void Shoot()
+    {
+        // Instantiate a new bullet from the bullet prefab
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+        // Set bullet properties directly on the bullet object
+        Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
+
+        // Set collision detection mode to ContinuousDynamic
+        bulletRigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+
+        bulletRigidbody.velocity = firePoint.TransformDirection(Vector3.forward) * projectileVelocity; // Set bullet velocity based on projectileVelocity
+        bullet.GetComponent<MeshRenderer>().material.color = Color.red; // Change bullet color to red
+
+        // Destroy the bullet after a certain time (optional)
+        Destroy(bullet, 5f);
+    }
+
+
 }
