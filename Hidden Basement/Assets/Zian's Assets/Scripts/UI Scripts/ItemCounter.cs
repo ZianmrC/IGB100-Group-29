a1edfe0941dd43f5daeb5528a5d47f18ea6885b3
Alex <alexinsh16@outlook.com>
2023-04-25 22:27:45 +1000

    key counter and interaction update

--- Hidden Basement/Assets/Zian's Assets/Scripts/UI Scripts/ItemCounter.cs
+++ Hidden Basement/Assets/Zian's Assets/Scripts/UI Scripts/ItemCounter.cs
@@ -1,22 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ItemCounter : MonoBehaviour
 {
 
+    public TMP_Text keyText;
+    //public TMP_Text keyCountText;
+    public int key = 0;
+    public int keycount = 0;
+
     private void Start()
     {
-        //scoreText.text = score.ToString();
+        keycount = PlayerPrefs.GetInt("Key " + "/5", 0);
+        keyText.text = "Keys " + key.ToString() + "/5";
+        //keyCountText.text = "Key: " + keycount.ToString() + "/5";
     }
 
     public void AddKey(int newKeyValue)
     {
-        //key += newKeyValue;
-       // keyText.text = key.ToString();
-        //if (keycount < key)
-       // {
-       //     PlayerPrefs.SetInt("")
-       // }
+        key += newKeyValue;
+        keyText.text = key.ToString();
+        if (keycount < key)
+        {
+            
+            PlayerPrefs.SetInt("Key",key);
+        }
     }
 }
