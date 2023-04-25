7cdd52b25d6fa366208d62a2baed3fd7f0e3c320
Alex <alexinsh16@outlook.com>
2023-04-25 15:30:35 +1000

    key counter

---
+++ Hidden Basement/Assets/Zian's Assets/Scripts/UI Scripts/ItemCounter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCounter : MonoBehaviour
+{
+
+    private void Start()
+    {
+        //scoreText.text = score.ToString();
+    }
+
+    public void AddKey(int newKeyValue)
+    {
+        //key += newKeyValue;
+       // keyText.text = key.ToString();
+        //if (keycount < key)
+       // {
+       //     PlayerPrefs.SetInt("")
+       // }
+    }
+}
