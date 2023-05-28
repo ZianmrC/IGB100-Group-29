72fb228fc7b4258564a290cb9fa4c77a4e24d850
Alex <alexinsh16@outlook.com>
2023-05-28 21:31:08 +1000

    m

--- Hidden Basement/Assets/DetectBook.cs
+++ Hidden Basement/Assets/DetectBook.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class DetectBook : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject book;
 
-    // Update is called once per frame
-    void Update()
+    public Transform here;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.gameObject == book)
+        {
+            GetComponent<PickUp>().transform.position = here.position;
+        }
     }
 }
