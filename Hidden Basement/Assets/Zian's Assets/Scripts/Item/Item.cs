5779973e33e27cc4e40d13449757ee05f6d4179e
Alex <alexinsh16@outlook.com>
2023-05-28 14:47:11 +1000

    .

--- Hidden Basement/Assets/Zian's Assets/Scripts/Item/Item.cs
+++ Hidden Basement/Assets/Zian's Assets/Scripts/Item/Item.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    public GameObject InteractableGameObject => gameObject;
+    //public GameObject InteractableGameObject => gameObject;
     
-    [SerializeField] private GameObject inHandItem;
+    //[SerializeField] private GameObject inHandItem;
 
-    [SerializeField] private Transform pickUpParent;
+    //[SerializeField] private Transform pickUpParent;
 
     //public string InterationPrompt => _prompt;
 
-    public bool Interact(Interactor interactor)
-    {
-        inHandItem = this.gameObject;
-        inHandItem.transform.position = Vector3.zero;
-        inHandItem.transform.rotation = Quaternion.identity;
-        inHandItem.transform.SetParent(pickUpParent.transform, false);
-        return true;
-    }
+    //public bool Interact(Interactor interactor)
+    //{
+    //    inHandItem = this.gameObject;
+    //    inHandItem.transform.position = Vector3.zero;
+    //    inHandItem.transform.rotation = Quaternion.identity;
+    //    inHandItem.transform.SetParent(pickUpParent.transform, false);
+   //     return true;
+   //}
 }
