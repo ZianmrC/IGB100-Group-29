6cd83d88e39065af8e06e18fd8f5e8e08a657317
Alex <alexinsh16@outlook.com>
2023-05-30 22:41:06 +1000

    pls don't eat my code github

---
+++ Hidden Basement/Assets/CameraMove.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    bool startNextRotation = true;
+    public bool rotRight;
+
+    public float yaw;
+
+    public float secondsToRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpStartRotation();
+    }
+
+    void Update()
+    {
+        if(startNextRotation && rotRight)
+        {
+            StartCoroutine(Rotate(yaw, secondsToRot));
+        }
+        else if(startNextRotation && !rotRight)
+        {
+            StartCoroutine(Rotate(-yaw, secondsToRot));
+        }
+    }
+
+    // Update is called once per frame
+    IEnumerator Rotate(float yaw, float duration)
+    {
+        startNextRotation = false;
+
+        Quaternion initialRotation = transform.rotation;
+
+        float timer = 0f;
+
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            transform.rotation = initialRotation * Quaternion.AngleAxis(timer / duration * yaw, Vector3.up);
+            yield return null;
+        }
+
+        //yield return new WaitForSeconds();
+
+        startNextRotation = true;
+        rotRight = !rotRight;
+    }
+
+    void SetUpStartRotation()
+    {
+        if(rotRight)
+        {
+            transform.localRotation = Quaternion.AngleAxis(-yaw / 2, Vector3.up);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.AngleAxis(yaw / 2, Vector3.up);
+        }
+    }
+}
